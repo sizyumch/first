fb0a8f6b4eb183e7f535449f9e2bf2fb94fefd30
sizyumch <sizyumch@gmail.com>
2013-09-29 19:08:05 +0300

    first commit

---
+++ src/main/java/first.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sasha
+ * Date: 26/09/13
+ * Time: 23:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class first
+{
+
+}
